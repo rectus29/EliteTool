473cbd0513f6a1d3be11773318f6f35d726efc14
a.bernard <a.bernard@e-deal.com>
2017-02-23 17:53:34 +0100

    adding log4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main extends Application {
 
+	private static final Logger log = LogManager.getLogger(Main.class);
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private String logPath="";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -29,5 +34,17 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
+        String logPath = "journal.log";
+
+		try(FileReader fr = new FileReader(logPath);){
+			BufferedReader br = new BufferedReader(fr);
+
+
+		} catch (Exception e) {
+			log.error(e);
+		}
+	}
+
+
+
 }
